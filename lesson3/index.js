e179c4da9a6ee93d0bbfd20b8a01291ca88fddc3
huy2321 <huy299925@gmail.com>
2023-09-10 14:06:02 +0700

    .

---
+++ lesson3/index.js
@@ -0,0 +1,46 @@
+// const h1 = document.querySelector(".h11")
+// console.log(h1)
+// h1.textContent="hoc ve DOM"
+// const p = document.querySelectorAll("button")
+// console.log(p)
+// const dangki= document.querySelector(".dangki")
+// console.log(dangki)
+// dangki.textContent='Đăng kí'
+// const p3= document.querySelector(".p")
+// console.log(p3)
+// p3.textContent="cung cấp kiến thức DOM"
+
+
+//css button
+// const button = document.querySelector(".dangki")
+// button.classList.add("button1")
+//bt
+// const h2 = document.querySelector(h2)
+// h2.classList.add(".title")
+// const p = document.querySelector(".subtile")
+// p.classList.remove("subtile")
+//bt
+// function student (name,age,scholl){
+
+// }
+// var animals = ['Monkey','Tiger','Elephant'];
+
+// function animals(array){
+//     let a = array[array.length-1]
+//     console.log(a)
+// }
+// animals(array)
+const button = document.querySelector(".dangki")
+
+function aler(){
+   
+const h1 = document.querySelector(".h11")
+h1.classList.add("red")
+
+}
+button.addEventListener("click",aler)
+h1.classList.remove("red")
+
+
+
+
